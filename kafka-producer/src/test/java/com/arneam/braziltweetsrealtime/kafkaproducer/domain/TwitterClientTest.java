5ff3a21f8f6781975f938dc6fc7de6041d19a842
Daniel Medeiros <daniel@arneam.com>
2019-11-25 05:52:19 -0300

    Add Twitter client dependency

---
+++ kafka-producer/src/test/java/com/arneam/braziltweetsrealtime/kafkaproducer/domain/TwitterClientTest.java
@@ -0,0 +1,11 @@
+package com.arneam.braziltweetsrealtime.kafkaproducer.domain;
+
+import org.junit.jupiter.api.Test;
+
+class TwitterClientTest {
+
+  @Test
+  void shouldCreateTwitterClient() {
+  }
+
+}
