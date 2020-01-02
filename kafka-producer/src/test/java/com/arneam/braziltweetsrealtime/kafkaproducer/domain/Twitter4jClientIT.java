41c60eb34513652a8a8c041bbc986eb3801f0197
Daniel Medeiros <daniel@arneam.com>
2020-01-01 22:20:40 -0300

    Remove unused code and imports

--- kafka-producer/src/test/java/com/arneam/braziltweetsrealtime/kafkaproducer/domain/Twitter4jClientIT.java
+++ kafka-producer/src/test/java/com/arneam/braziltweetsrealtime/kafkaproducer/domain/Twitter4jClientIT.java
@@ -2,10 +2,7 @@ package com.arneam.braziltweetsrealtime.kafkaproducer.domain;
 
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.greaterThan;
-import static org.hamcrest.core.Is.is;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class Twitter4jClientIT {
@@ -20,7 +17,6 @@ public class Twitter4jClientIT {
     Thread.sleep(10000);
 
     assertThat(client, notNullValue());
-//    assertThat(client.messages().size(), is(greaterThan(0)));
   }
 
 }
